fea3105574ba35b6915784befd9f5dfde7d0162c
xiaoqiu <362442015@qq.com>
2023-02-22 20:23:15 +0800

    third commit

--- src/main/java/com/my/git/GitTest.java
+++ src/main/java/com/my/git/GitTest.java
@@ -2,8 +2,8 @@ package com.my.git;
 
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("hello");
-        System.out.println("hello");
-        System.out.println("hello");
+        System.out.println("hello git1");
+        System.out.println("hello git2");
+        System.out.println("hello git3");
     }
 }
