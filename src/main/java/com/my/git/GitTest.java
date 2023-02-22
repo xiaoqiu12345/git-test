1accf1876a3cd5c50c3b75b9a055f0cd427a5a97
xiaoqiu <362442015@qq.com>
2023-02-22 20:22:21 +0800

    second commit

--- src/main/java/com/my/git/GitTest.java
+++ src/main/java/com/my/git/GitTest.java
@@ -3,5 +3,7 @@ package com.my.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
     }
 }
